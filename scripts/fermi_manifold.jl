1588ac5b1c8c52e60fd8d80904d5e804ce430610
awage <alexandre.wagemakers@gmail.com>
2025-04-12 10:38:27 +0200

    change keywords

--- scripts/fermi_manifold.jl
+++ scripts/fermi_manifold.jl
@@ -14,8 +14,8 @@ using ProgressMeter
 include(srcdir("utils.jl"))
 
 
-function get_branch_number(v0, y_init, xs, num_rays, K; prefix = "fermi_dec", force = false, a, dot_radius, softness, θ_range) 
-    d = @dict(v0, y_init, num_rays, xs, K, a, dot_radius, softness, θ_range)  
+function get_branch_number(v0, y_init, xs, num_rays, K, T; prefix = "fermi_dec", force = false, a, dot_radius, softness, θ_range) 
+    d = @dict(v0, y_init, num_rays, xs, K, a, dot_radius, T, softness, θ_range)  
     
     data, file = produce_or_load(
         datadir("storage"), # path
@@ -72,7 +72,6 @@ function manifold_track(xs, ys, y_init, dyr, potential; K = 1.)
     dy = ys[2] - ys[1]
     num_rays = length(y_init)
     width = length(xs)
-    # height = length(ys)
     hst = zeros(width)
     nb_br = zeros(width)
     ray_y = deepcopy(collect(y_init))
@@ -94,22 +93,23 @@ end
 
 
 
-v0 = 0.1; dt = 0.01; num_rays = 200000; n_avg = 40; K = 1.; T = 1500
+v0 = 0.1; dt = 0.01; num_rays = 200000; n_avg = 40; K = 1.; T = 100
 y_init = range(-40*0.2, 40*0.2, length = num_rays)
 ys = y_init[findall(0 .≤ y_init .≤ 1.)]
-xs = range(0,40., step = dt)
+xs = range(0,T, step = dt)
 v_range = logrange(0.001, 1, length = 20)
 a = 0.2; dot_radius = 0.2*0.25; softness = 0.2; θ_range = range(0,π/4, length = n_avg) 
 
 for v0 in v_range
-    dat = get_branch_number(v0, y_init, xs, num_rays, K; prefix = "fermi_br_lyap", 
-                           a, dot_radius, softness, θ_range, force = true) 
+    dat = get_branch_number(v0, y_init, xs, num_rays, K, T; prefix = "fermi_br_lyap", 
+                           a, dot_radius, softness, θ_range, force = false) 
     @unpack hst_v_all,nb_v_all = dat
     pargs = (yticklabelsize = 30, xticklabelsize = 30, ylabelsize = 30, xlabelsize = 30) 
     fig = Figure(size=(800, 1200))
     ax2= Axis(fig[1, 1]; xlabel = L"t", ylabel = L"N_{b}", pargs...) 
     lines!(ax2, xs, mean(nb_v_all, dims = 1)[1]; color = :blue, label = "all rays")
-    save(plotsdir(savename("plot_nb_",(v0, num_rays), ".pdf")),fig)
+    save(plotsdir(savename("plot_nb",@dict(v0, num_rays), "pdf")),fig)
+    println("\\includegraphics[width=0.3\\textwidth]{../plots/",savename("plot_nb",@dict(v0, num_rays), "pdf"),"}")
 end
 
 
